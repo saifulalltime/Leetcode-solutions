e0e561db1bf4b42d886cfe4dda53ee314c33411c
Saiful Islam <saifulalltime@gmail.com>
2024-05-22 19:11:57 +0600

    - Next Greater element

--- 496NextGreaterElementI.py
+++ 496NextGreaterElementI.py
@@ -4,38 +4,22 @@ def generate(nums1, nums2):
     ans = []
     
     for i in nums1:
-        c=0
+        detector = False
         for j in range(nums2.index(i) + 1, len(nums2)):
             if nums2[j] > i:
                 ans.append(nums2[j])
-                c+=1
+                detector = True
                 break
 
-        if c == 0:
+        if not detector:
             ans.append(-1)   
     return ans     
 
 # nums1 = [1,3,5,2,4]
-# nums2 = [6,5,4,3,2,1,7]
+# nums2 = [6,5,4,3,2,1,7]                                                     
 nums1 = [4,1,2]
 nums2 = [1,3,4,2]
 
 
 callValue = generate(nums1, nums2)
 print(callValue)   
-
-#   ans = []
-    
-#         for i in range(len(nums1)):
-#             for j in range(1,len(nums2)):
-#                 # print("j",j,"=",nums2[j-1],"==",nums2[j])
-#                 # print(nums1[i] ,"==", nums2[j-1])
-                
-#                 if nums1[i] == nums2[j-1]:
-#                     if nums2[j] > nums2[j-1]:
-#                         ans.append(nums2[j])
-#                     else:
-#                         ans.append(-1)   
-#                 elif j == len(nums2)-1 and nums1[i] == nums2[len(nums2)-1]:
-#                     ans.append(-1)   
-#         return ans 
